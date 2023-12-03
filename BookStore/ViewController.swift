5d887c6fdc76e7f02ee1ecf6c7a5c932679167eb
Dmitry Lorents <zmey.task@gmail.com>
2023-12-03 07:12:28 +0300

    Initial Commit

---
+++ BookStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStore
+//
+//  Created by Dmitry on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
