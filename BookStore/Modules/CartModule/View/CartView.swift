42ae5981c01c74a902800c60d7dcb2c25eaf42f9
Nikita Kornuk <kornyk62@gmail.com>
2023-12-06 21:21:19 +0100

    add CartView and ViewController

--- BookStore/Modules/CartModule/View/CartView.swift
+++ BookStore/Modules/CartModule/View/CartView.swift
@@ -7,10 +7,45 @@ import UIKit
 class CartView: UIView {
 
     // MARK: - Parameters
-   
+    private lazy var tableView:UITableView={
+        let element = UITableView()
+        element.register(CartViewCell.self, forCellReuseIdentifier: "CartViewCell")
+        element.separatorStyle = .none
+        element.estimatedRowHeight = 148
+        element.allowsSelection = false
+        element.showsVerticalScrollIndicator = false
+        element.backgroundColor = .clear
+        return element
+    }()
     //MARK: - Init
-   
+   override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     //MARK: - Methods
-   
+    private func setupViews(){
+        self.addSubview(tableView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self.safeAreaLayoutGuide)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
+//MARK: - Extensions
+extension CartView{
+    func transferDelegates(dataSource: UITableViewDataSource, delegate: UITableViewDelegate){
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
 }
