e6cf60ab9ecc9e3e932405827b093511cadaa3d5
Dmitry Lorents <zmey.task@gmail.com>
2023-12-07 21:26:05 +0300

    Merge manually privetnikita/CartVC into develop

--- BookStore/Modules/CartModule/Controller/CartViewController.swift
+++ BookStore/Modules/CartModule/Controller/CartViewController.swift
@@ -1,17 +1,41 @@
 //
-//  settingsViewController.swift
+//  CartViewController.swift
 //
 
 import UIKit
+import SnapKit
 
 class CartViewController: UIViewController {
     
     // MARK: - Parameters
     
+    private let viewCart = CartView()
+    private var likeArray = Source.getCart()
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .systemBackground
+        view = viewCart
+        viewCart.transferDelegates(dataSource: self, delegate: self)
     }
+}
+
+//MARK: - Extensions
+extension CartViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        likeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CartViewCell.reuseID, for: indexPath) as? CartViewCell else { fatalError() }
+
+        cell.configureCell(likeBook: likeArray[indexPath.row])
+        return cell
+    }
+}
+
+extension CartViewController: UITableViewDelegate {
+
 }
