37ff9f28960d8c6d698019980f07dc35a77ad004
Dmitry Lorents <zmey.task@gmail.com>
2023-12-13 20:57:09 +0300

    Fix Nikitas problem

--- BookStore/Modules/CartModule/Model/CartModel.swift
+++ BookStore/Modules/CartModule/Model/CartModel.swift
@@ -14,8 +14,8 @@ struct CartModel{
 struct Source{
     static func getCart() ->[Book] {
         [
-            Book(key: "key1", name: "name1", author: "author1", category: "category1", imageID: 1),
-            Book(key: "key2", name: "name2", author: "author2", category: "category2", imageID: 2),
+            Book(key: "key1", name: "name1", author: "author1", category: "category1", imageID: 1, rating: 3.3),
+            Book(key: "key2", name: "name2", author: "author2", category: "category2", imageID: 2, rating: 5.0),
         ]
     }
 }
