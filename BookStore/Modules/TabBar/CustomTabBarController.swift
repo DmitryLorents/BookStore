ca92c64018da8c430c97e80e41edfd7f2f491817
Nikita Kornuk <kornyk62@gmail.com>
2023-12-13 16:04:09 +0100

    add mark

--- BookStore/Modules/TabBar/CustomTabBarController.swift
+++ BookStore/Modules/TabBar/CustomTabBarController.swift
@@ -40,6 +40,7 @@ class CustomTabBarController: UITabBarController {
             selectedImage: UIImage(named: "categoriesSelected"))
         
         let likesVC = generateVC(
+            //TODO: - в бук другую модель поставить
             viewController: CartViewController(book: Source.getCart()),
             image: UIImage(named: "likesUnselected"),
             selectedImage: UIImage(named: "likesSelected"))
