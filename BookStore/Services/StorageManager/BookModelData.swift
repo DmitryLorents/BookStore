67867616ba238691608492276d655a0eca19b38a
Nikita Kornuk <kornyk62@gmail.com>
2023-12-13 22:50:09 +0100

    add Realm

---
+++ BookStore/Services/StorageManager/BookModelData.swift
@@ -0,0 +1,8 @@
+//
+//  BookModelData.swift
+//  BookStore
+//
+//  Created by Admin on 13.12.2023.
+//
+
+import Foundation
---
+++ BookStore/Services/StorageManager/StorageManagerRealm.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManagerRealm.swift
+//  BookStore
+//
+//  Created by Admin on 13.12.2023.
+//
+
+import Foundation
