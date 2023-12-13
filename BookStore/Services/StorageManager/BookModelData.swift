33a7c5363be779fcf009ff3357fa7f41913c4085
Nikita Kornuk <kornyk62@gmail.com>
2023-12-13 22:51:00 +0100

    add bookModel

--- BookStore/Services/StorageManager/BookModelData.swift
+++ BookStore/Services/StorageManager/BookModelData.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class BookModelData: Object {
+    @Persisted var keyForServer: String
+    @Persisted var name: String
+    @Persisted var author: String
+    @Persisted var category: String
+    @Persisted var imageID: Int
+    @Persisted var rating: Double
+    
+    convenience init(key: String, name: String, author: String, category: String, imageID: Int, rating: Double) {
+        self.init()
+        self.keyForServer = key
+        self.name = name
+        self.author = author
+        self.category = category
+        self.imageID = imageID
+        self.rating = rating
+    }
+}
