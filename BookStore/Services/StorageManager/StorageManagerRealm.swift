6c474978fd18aaadadfe155c156a7afcee4dab99
Nikita Kornuk <kornyk62@gmail.com>
2023-12-16 01:15:20 +0100

    changed placement in array

--- BookStore/Services/StorageManager/StorageManagerRealm.swift
+++ BookStore/Services/StorageManager/StorageManagerRealm.swift
@@ -45,7 +45,7 @@ class StorageManagerRealm{
         var books = [Book]()
         for bookModel in bookModels {
             let book = transferBookModelToBook(bookModel)
-            books.append(book)
+            books.insert(book, at: 0)
         }
         return books
     }
