db61d4d45677887d09912649a3bb55b44da2c0d1
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-12-16 13:27:06 +0200

    add read button logic + ScrollView hotFix

--- BookStore/Modules/ProductModule/Controller/ProductViewController.swift
+++ BookStore/Modules/ProductModule/Controller/ProductViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ProductViewController: UIViewController {
     //MARK: - Parameters
@@ -76,5 +77,12 @@ class ProductViewController: UIViewController {
         guard let book else {return}
         storageManager.deleteBook(withBook: book)
         updateFavouriteBarButton(false)
+    }
+    
+    @objc func openBookWebPage() {
+        guard let key = book?.key else {return}
+        let descriptionURL = OpenLibraryEndpoints.returnDescriptionURL(key: key)
+        let webController = SFSafariViewController(url: descriptionURL)
+        present(webController, animated: true)
     }
 }
